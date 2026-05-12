929f2deb557d3dd8fe55e7865074617511dcf687
YuveerKal <69754708+YuveerK@users.noreply.github.com>
2026-05-12 02:39:57 +0200

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { env } from "node:process";
 
+const repositoryName = env.GITHUB_REPOSITORY?.split("/")[1];
+const githubPagesBase = repositoryName ? `/${repositoryName}/` : "/portfolio/";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: env.GITHUB_PAGES === "true" ? "/yuveer-kallideen-website/" : "/",
+  base: env.GITHUB_PAGES === "true" ? githubPagesBase : "/",
   plugins: [react(), tailwindcss()],
 });
